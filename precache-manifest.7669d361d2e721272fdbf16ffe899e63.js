8987541e6e20a890605cf42bd67717e8cedbb1a6
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2019-12-20 13:41:03 -0500

    Updates

---
+++ precache-manifest.7669d361d2e721272fdbf16ffe899e63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40588f58f33465eac39aad1d77e98c5a",
+    "url": "/Evies_Match_App/index.html"
+  },
+  {
+    "revision": "67b82cc4696b1abcde6c",
+    "url": "/Evies_Match_App/static/css/main.cd0d757d.chunk.css"
+  },
+  {
+    "revision": "b733c9f1db3effe30810",
+    "url": "/Evies_Match_App/static/js/2.f0ae6d09.chunk.js"
+  },
+  {
+    "revision": "67b82cc4696b1abcde6c",
+    "url": "/Evies_Match_App/static/js/main.c0e91cbd.chunk.js"
+  },
+  {
+    "revision": "e73692017d7379ec951e",
+    "url": "/Evies_Match_App/static/js/runtime-main.ebe11ca6.js"
+  }
+]);
